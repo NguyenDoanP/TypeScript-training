fc96383ded98ade67ccc34a9470eba394a810e08
Nguyen Doan <doanpnguyen.dev@gmail.com>
2026-06-22 12:03:02 +0700

    feat: example variable-declerations

---
+++ object-destructuring.ts
@@ -0,0 +1,9 @@
+const person = {
+  name: 'Alice',
+  age: 25,
+};
+
+const { name: personName, age: personAge } = person;
+
+console.log(personName);
+console.log(personAge);
---
+++ spread.ts
@@ -0,0 +1,7 @@
+const firstSpread = [1, 2];
+
+const secondSpread = [3, 4];
+
+const result = [...firstSpread, ...secondSpread];
+
+console.log(result);
